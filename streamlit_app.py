1d025c87b0d45ec3162401ff14e4be7dfea55720
littlecm <clittle.norml@gmail.com>
2024-04-30 18:09:30 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from io import StringIO
 # Constants
 VINSOLUTIONS_BASE_URL = "https://feeds.amp.auto/feeds/vinsolutions/"
 DEALERDOTCOM_URL = "https://feeds.amp.auto/feeds/coxautomotive/dealerdotcom.csv"
-API_BASE_URL = "https://cws.gm.com/vs-cws/vehshop/v2/vehicle"
+API_URL = "https://hook.us1.make.com/ae3j7yvt16erp88q4ygnb4kh0n9xg6qq"
 
 # Helper functions
 def download_csv(url):
@@ -18,22 +18,8 @@ def download_csv(url):
         return pd.DataFrame()
 
 def get_api_data(vin):
-    api_url = f"{API_BASE_URL}?vin={vin}&postalCode=48640&locale=en_US"
-    response = requests.get(api_url, headers={
-        "authority": "cws.gm.com",
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        "accept-language": "en-US,en;q=0.9",
-        "cache-control": "max-age=0",
-        "sec-ch-ua": '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": '"Windows"',
-        "sec-fetch-dest": "document",
-        "sec-fetch-mode": "navigate",
-        "sec-fetch-site": "none",
-        "sec-fetch-user": "?1",
-        "upgrade-insecure-requests": "1",
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
-    })
+    payload = {"vin": vin}
+    response = requests.post(API_URL, json=payload)
     if response.ok:
         return response.json()
     else:
@@ -92,23 +78,12 @@ if st.button("Reconcile Data"):
     for vin in unique_vinsolutions.union(unique_dealerdotcom):
         api_data = get_api_data(vin)
         if api_data:
-            if "mathBox" in api_data and "recallInfo" in api_data["mathBox"] and "This vehicle is temporarily unavailable" in api_data["mathBox"]["recallInfo"]:
+            if api_data.get("status") == "recall":
                 results.append({'VIN': vin, 'Result': "Vehicle with Recall"})
-                continue
-            if "inventoryStatus" in api_data:
-                inventory_status = api_data["inventoryStatus"].get("name")
-                if inventory_status:
-                    if inventory_status == "Rtl_Intrans" and vin in unique_dealerdotcom:
-                        results.append({'VIN': vin, 'Result': "In Transit - Not expected in HomeNet"})
-                    elif inventory_status == "EligRtlStkCT":
-                        results.append({'VIN': vin, 'Result': "Courtesy Vehicle"})
-                    else:
-                        results.append({'VIN': vin, 'Result': f"Other Inventory Status: {inventory_status}"})
-                else:
-                    if vin in unique_dealerdotcom:
-                        results.append({'VIN': vin, 'Result': "Exclusive to Dealer.com Website"})
-                    else:
-                        results.append({'VIN': vin, 'Result': "Exclusive to HomeNet"})
+            elif api_data.get("status"):
+                results.append({'VIN': vin, 'Result': api_data.get("status")})
+            else:
+                results.append({'VIN': vin, 'Result': "Status Unknown"})
         else:
             results.append({'VIN': vin, 'Result': "API request failed"})
 
